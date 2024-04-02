dec5f2042e028431f3e00c56de67ae0ba8c71b45
Kurnia Ramadhan <96585178+blogramadhan@users.noreply.github.com>
2024-04-02 09:47:15 +0700

    update code

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ app.register_blueprint(epurchasing_bp, url_prefix='/epurchasing')
 app.register_blueprint(monitoring_bp, url_prefix='/monitoring')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port='1234')
