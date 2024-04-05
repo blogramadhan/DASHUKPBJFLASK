2eedc2890ade71c426748f2cea51c31306fe14dc
Kurnia Ramadhan <96585178+blogramadhan@users.noreply.github.com>
2024-04-05 09:34:34 +0700

    update code

--- app.py
+++ app.py
@@ -1,20 +1,5 @@
-from flask import Flask
-import pandas as pd
-import duckdb
-from home import home_bp
-from sirup import sirup_bp
-from spse import spse_bp
-from epurchasing import epurchasing_bp
-from monitoring import monitoring_bp
+from websites import create_app
 
-app = Flask(__name__)
-
-# Daftarkan Blueprint
-app.register_blueprint(home_bp)
-app.register_blueprint(sirup_bp, url_prefix='/sirup')
-app.register_blueprint(spse_bp, url_prefix='/spse')
-app.register_blueprint(epurchasing_bp, url_prefix='/epurchasing')
-app.register_blueprint(monitoring_bp, url_prefix='/monitoring')
-
-if __name__ == '__main__':
-    app.run(debug=True, port='1234')
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True, port=1234)
